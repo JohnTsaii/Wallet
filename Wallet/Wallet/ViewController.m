fb341156e524fee4bcc32fd2da5f4b2f48c9c86a
John Tsai <caijingzhuang@gmail.com>
2016-03-22 21:32:53 +0800

    add new project

---
+++ Wallet/Wallet/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Wallet
+//
+//  Created by John TSai on 16/3/22.
+//  Copyright © 2016年 JohnTsai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
