3d0ed8064d62c68f98ca32bbc0092cac249c7b4e
John Tsai <caijingzhuang@gmail.com>
2016-04-28 14:57:34 +0800

    add app icon

---
+++ Wallet/Wallet/Features/Home/ViewController/WTAccountingViewController.h
@@ -0,0 +1,13 @@
+//
+//  WTAccountingViewController.h
+//  Wallet
+//
+//  Created by John TSai on 16/4/28.
+//  Copyright © 2016年 JohnTsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTAccountingViewController : UIViewController
+
+@end
---
+++ Wallet/Wallet/Features/Home/ViewController/WTAccountingViewController.m
@@ -0,0 +1,45 @@
+//
+//  WTAccountingViewController.m
+//  Wallet
+//
+//  Created by John TSai on 16/4/28.
+//  Copyright © 2016年 JohnTsai. All rights reserved.
+//
+
+#import "WTAccountingViewController.h"
+
+@implementation WTAccountingViewController
+#pragma mark - life cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Delegate & DataSource
+
+#pragma mark - event response
+
+#pragma mark - private method
+
+#pragma mark - getters and setters
+@end
